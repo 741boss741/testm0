2c869c4b7b82f99a1ef6178a40f0af8727693f66
741boss741 <47608727+741boss741@users.noreply.github.com>
2019-05-18 05:45:02 +0200

    bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setActivity('', { type: 'PLAYING' })
+});
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+ 
+ 
+client.login('token');
